e7a36cb149b8eb9f968d1121690a0e380f6770ad
ironapps <55113166+ironapps@users.noreply.github.com>
2019-11-15 17:41:39 -0700

    Update MsgDiscord.js

--- MsgDiscord.js
+++ MsgDiscord.js
@@ -1,3 +1,4 @@
+//install//
 document.write( '<script src=\"http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>' );
 window.onload = function WindowLoad(event) {
     document.write( '<script src=\"https://ironapps.github.io/Iron4apps/RealmBot.js\"></script>\n' );
